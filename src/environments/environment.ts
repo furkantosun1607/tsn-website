b8c7534d5e4ba5b5cac4f0a1deffab49644ad280
Furkan Tosun <furkantosun1607@gmail.com>
2026-03-09 00:19:11 +0300

    update api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   googleClientId: '179638045847-tb7ea2053753c98saneg25pd4u9q111b.apps.googleusercontent.com',
-  apiUrl: 'http://localhost:8000/api/v1'
+  apiUrl: 'https://jonell-submental-punditically.ngrok-free.dev/api/v1'
 };
